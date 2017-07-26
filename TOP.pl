a84c398d5f3b7a30abbaa2cab81c17cf65bdce2d
Kuiliang Lin <b9907020@mail.ntust.edu.tw>
2017-07-26 11:57:33 +0800

    Update TOP.pl

--- TOP.pl
+++ TOP.pl
@@ -2,13 +2,21 @@
 use warnings; use strict;
 use LWP::Simple;
 
+#system "perl name.pl yearstart yearend monthstart monthend";
 
-print "==>run history.pl\n";
-system "perl History.pl";
 
-=header
-print "==>run hash.pl\n";
-system "perl Hash.pl";
-=cut
+#system "perl GetStockNumber.pl";
+system "perl History_tse.pl 2017 2017 2 2";
+system "perl HistoryBasic.pl";
+system "perl HistoryBasic_2.pl 2017 2017 2 2";
+system "perl HistoryBasic_3.pl 2017 2017";
+#system "perl Hash.pl";
 
-print "\n ~~~~ All works have been done! ~~~~ \n\n";
+
+
+
+
+print "\n ~~~~ TOP => All works have been done! ~~~~ \n\n";
+
+
+#chomp($input=<STDIN>);
