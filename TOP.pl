a85557067550403c48394e788ec5a44f57adde54
Kuiliang Lin <b9907020@mail.ntust.edu.tw>
2017-07-27 12:01:46 +0800

    Update TOP.pl

--- TOP.pl
+++ TOP.pl
@@ -2,14 +2,39 @@
 use warnings; use strict;
 use LWP::Simple;
 
-#system "perl name.pl yearstart yearend monthstart monthend";
+=header
+system "perl name.pl yearstart yearend monthstart monthend";
+daily : acredit foreign netbuy skill zhuli
+month : asale
+season : balance income
+year : dividend finicial
+=cut
 
 
 #system "perl GetStockNumber.pl";
-system "perl History_tse.pl 2017 2017 2 2";
+
+################################ 
+###### daily : skill
+################################ 
+system "perl History_tse.pl 2016 2016 2 2"; 
+
+################################ 
+##### year : dividend finicial 
+##### season : balance income
+################################ 
 system "perl HistoryBasic.pl";
-system "perl HistoryBasic_2.pl 2017 2017 2 2";
-system "perl HistoryBasic_3.pl 2017 2017";
+
+################################ 
+##### daily : acredit foreign netbuy zhuli
+################################ 
+system "perl HistoryBasic_2.pl 2016 2016 2 2"; 
+
+################################ 
+##### month : asale
+################################ 
+system "perl HistoryBasic_3.pl 2016 2016"; 
+
+
 #system "perl Hash.pl";
 
 
