3ad87f1139f55eb62cff639f428784e7c7507a58
Kuiliang Lin <b9907020@mail.ntust.edu.tw>
2017-07-31 10:03:39 +0800

    Update TOP.pl

--- TOP.pl
+++ TOP.pl
@@ -10,23 +10,29 @@ season : balance income
 year : dividend finicial
 =cut
 
+my $yearstart = 2000;
+my $yearend = 2017;
+my $monthstart = 1;
+my $monthend = 12;
+
+
 
 #system "perl GetStockNumber.pl";
 
 ################################ 
 ###### daily : skill
 ################################ 
-system "perl History_tse.pl 2016 2016 2 2"; 
+system "perl History_tse.pl $yearstart $yearend $monthstart $monthend"; 
 
 ################################ 
 ##### daily : acredit foreign netbuy zhuli
 ################################ 
-system "perl HistoryBasic_2.pl 2016 2016 2 2"; 
+system "perl HistoryBasic_2.pl $yearstart $yearend $monthstart $monthend"; 
 
 ################################ 
 ##### month : asale
 ################################ 
-system "perl HistoryBasic_3.pl 2016 2016"; 
+system "perl HistoryBasic_3.pl $yearstart $yearend"; 
 
 ################################ 
 ##### year : dividend finicial 
